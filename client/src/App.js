071ea2476c9e9650127195ac5870d6cedf763138
Sanchit <sanchit.dang03@gmail.com>
2022-12-20 00:03:59 +0530

    mongo

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+// Ract
+// to start 'npm start'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+function App() {
+  
+  return (
+    <div> 
+      <BrowserRouter>
+      <Routes>
+        <Route path='/Login' element={<Login/>} />
+        <Route path='/Register' element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,15 +1,41 @@
-// Express + Node
-// to start 'npm run dev'
-
-//import express from 'express'        --ES6
-//or
 const express = require('express')
-
 const app = express ()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
 
+app.use(cors())
+app.use(express.json())
 
-app.get('/hello', (req, res) => {
-    res.send('hola')
+mongoose.connect('mongodb://localhost:27017/UsersDatabase')
+
+app.post('/api/register', async (req, res) => {
+    console.log(req.body)
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({status : 'ok'})
+    } catch (err) {
+        console.log(err)
+        res.json({status : 'error', error: 'Duplicate email'})
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+
+        if (user) {
+            return res.json({status : 'ok', user: true})
+        } else {
+            res.json({status : 'error', user: false})
+        }
+
 })
 
 app.listen(1337, () => {
